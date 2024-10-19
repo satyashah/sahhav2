81e2a0347fb774e30dd2cc05ba991572a43f2c60
Satya Shah <satyashah@wireless-10-104-177-51.umd.edu>
2024-10-19 16:05:10 -0400

    v1

---
+++ app/index.tsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react';
+import { Text } from 'react-native';
+import { SafeAreaView } from 'react-native'
+import Sahha, { SahhaEnvironment, SahhaSensor } from 'sahha-react-native';
+
+
+const sahhaSettings = {
+    environment: SahhaEnvironment.sandbox,
+
+    // sensors: [SahhaSensor.pedometor] // not worki
+
+    // sensors: null,
+
+    sensors: [
+        SahhaSensor.heart_rate_variability_rmssd,
+        SahhaSensor.active_energy_burned,
+        SahhaSensor.sleep,
+        // SahhaSensor.
+    ],
+}
+
+function App(): JSX.Element {
+
+    const [sahhaConfigured, setSahhaConfigured] = useState(false);
+
+    useEffect(() => {
+        Sahha.configure(sahhaSettings, (error, success) => {
+            console.log(`SahhaConfig success: ${success}`);
+            setSahhaConfigured(success);
+
+            if (error) {
+                console.log(`SahhaConfig error: ${error} `);
+            }
+        });
+    }, []);
+    if (!sahhaConfigured) return <></>;
+
+    return (
+        <SafeAreaView>
+            <Text>HEllO</Text>
+        </SafeAreaView>
+    );
+}
+
+export default App;
